6a43097905e42b3d7e237403e2c0f6b9bd19eea0
Nick Schulze <nickschulze@MacBook-Pro.local>
2015-07-05 13:11:48 -0500

    javascripts

---
+++ myscript.js
@@ -0,0 +1,19 @@
+function scrollToEvent()
+{
+    $(window).scrollTop(620);
+}
+
+function scrollToHonorees()
+{
+    $(window).scrollTop(1720);
+}
+
+function scrollToSponsorship()
+{
+    $(window).scrollTop(2320);
+}
+
+function scrollToMDA()
+{
+    $(window).scrollTop(3920);
+}
